415da3e432f559ad4ee22fe6cf9a32c8a4deed3a
Kennedy Musau <kennedy.musauu@gmail.com>
2023-08-17 23:48:05 +0300

    include controller associations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,12 @@
 class UsersController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+  def show
+    user = User.find_by(id: params[:id])
+    render json: user, include: :items, status: :ok
+  end
+
+  def render_not_found_response
+    render json: { error: "user not found" }
+  end
 end
